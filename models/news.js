91917126738536feef5ce5b23b7b6750e429f27a
Natalya Babenko <natapervomaisk88@gmail.com>
2022-10-06 10:35:36 +0300

    added delete

--- models/news.js
+++ models/news.js
@@ -1,8 +1,8 @@
 const news = [
-  { title: "News 1", text: "Text for News 1" },
-  { title: "News 2", text: "Text for News 2" },
-  { title: "News 3", text: "Text for News 3" },
-  { title: "News 4", text: "Text for News 4" },
-  { title: "News 5", text: "Text for News 5" },
+  { id: 12, title: "News 1 test", text: "Text for News 1" },
+  { id: 34, title: "News 2", text: "Text for News 2" },
+  { id: 35, title: "News 3", text: "Text for News 3" },
+  { id: 543, title: "News 4", text: "Text for News 4" },
+  { id: 5, title: "News 5", text: "Text for News 5" },
 ];
 export default news;
--- routers/routers.js
+++ routers/routers.js
@@ -1,18 +1,27 @@
 import Router from "express";
 import path from "path";
 import news from "../models/news.js";
-import ejs from "ejs";
+import methodOverride from "method-override";
 const __dirname = path.resolve();
 const router = Router();
+router.use(methodOverride("X-HTTP-Method")); //          Microsoft
+router.use(methodOverride("X-HTTP-Method-Override")); // Google/GData
+router.use(methodOverride("X-Method-Override")); //      IBM
+router.use(
+  methodOverride(function (req, res) {
+    if (req.body && typeof req.body === "object" && "_method" in req.body) {
+      // look in urlencoded POST bodies and delete it
+      const method = req.body._method;
+      delete req.body._method;
+      return method;
+    }
+  })
+);
 router
   .route("/")
   .get((req, res) => {
     //res.sendFile(path.resolve(__dirname, "views", "index.html"));
-    res.format([
-      {
-        html: res.render("index.ejs", { title: "My Express (ejs)" }),
-      },
-    ]);
+    res.render("index.ejs", { title: "My Express (ejs)", news: news });
   })
   .post((req, res) => {
     res.send("<h1>Express POST REQUEST</h1>");
@@ -24,16 +33,32 @@ router
     res.send(news);
   })
   .post((req, res) => {
+    let biggest = news.reduce((prev, current) =>
+      prev.id > current.id ? prev : current
+    );
     news.push({
+      id: biggest.id + 1,
       title: req.body.title,
       text: req.body.text,
     });
-    res.redirect("/news");
+    res.redirect("/");
   });
 
-router.route("/news/:id").get((req, res) => {
-  res.send(news[req.params.id - 1]);
-});
+router
+  .route("/news/:id")
+  .get((req, res) => {
+    console.log("TEST");
+    let obj = news.find((el) => el.id === parseInt(req.params.id));
+    res.send(obj);
+  })
+  .delete((req, res) => {
+    let obj = news.find((el) => el.id === parseInt(req.params.id));
+    if (obj) {
+      let i = news.indexOf(obj);
+      news.splice(i, 1);
+    }
+    res.send(news);
+  });
 
 /*
   /news GET - получить все новости
